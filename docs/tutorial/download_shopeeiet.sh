ff22073d55d903b846f88be8993ab6e5ed66a987
Hang Zhang <hzaws@amazon.com>
2019-12-01 20:53:39 -0800

    Add files via upload

--- docs/tutorial/download_shopeeiet.sh
+++ docs/tutorial/download_shopeeiet.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-mkdir ./data
-mkdir ./data/shopeeiet
+mkdir -p ./data
+mkdir -p ./data/shopeeiet
+mkdir -p ./data/shopeeiet/train
 cd ./data/shopeeiet/
 kaggle competitions download -c shopee-iet-machine-learning-competition
-unzip Training%20Images.zip
-unzip Test%20Images.zip
+unzip shopee-iet-machine-learning-competition.zip
+mv Training\ Images.zip train && cd train && unzip Training\ Images.zip
+cd .. && unzip Test\ Images.zip
 mv Test test
-rm Training%20Images.zip Test%20Images.zip sample%20submission.csv
+rm *.zip *.csv train/*.zip
