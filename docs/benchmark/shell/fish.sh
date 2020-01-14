b6de1d916d2e5679f5714882dd4be2bb4a0ac0af
aptsunny <aptsunny@tongji.edu.cn>
2020-01-14 06:44:04 +0000

    update script

--- docs/benchmark/shell/fish.sh
+++ docs/benchmark/shell/fish.sh
@@ -1,5 +1,4 @@
 # 30_5
-python benchmark.py --data-dir data --dataset fisheries_Monitoring --num-trials 30 --num-epochs 5 --batch-size 48 --ngpus-per-trial 8 
+python benchmark.py --data-dir data --dataset fisheries_Monitoring --num-trials 30 --num-epochs 5 --batch-size 96 --ngpus-per-trial 8 
 # 180_1
-python benchmark.py --data-dir data --dataset fisheries_Monitoring --num-trials 1 --num-epochs 180 --batch-size 48 --ngpus-per-trial 1 --resume --submission
-
+python benchmark.py --data-dir data --dataset fisheries_Monitoring --num-trials 1 --num-epochs 120 --batch-size 96 --ngpus-per-trial 1 --resume --submission
